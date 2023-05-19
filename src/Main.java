3f7f44323b8e1881c826a0301b8f6785e9a0c188
Olesya <lesya.garsiya@gmail.com>
2023-05-19 18:31:13 +0500

    3/7

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Task 1");
+
+        for (int i = 0; i <= 10; i += 1) {
+            System.out.println("Интерация цикла " + i);
+        }
+
+        System.out.println("Task 2");
+
+        for (int i = 10; i >= 0; i -= 1) {
+            System.out.println("Интерация цикла " + i);
+        }
+
+        System.out.println("Task 3");
+
+        for (int i = 0; i <= 17; i += 2) {
+            System.out.println("Интерация цикла " + i);
+        }
+    }
+}
